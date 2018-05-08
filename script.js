fd303521b60642b4eb96ae5c62cb54f8cfa1aafe
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-05-08 11:27:58 +0200

    Add content to zad.5

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 //Zad.1
-
 const first = 'Hello';
 const second = 'World';
 const all = `${first} ${second}`;
 console.log(all)
 
+
 //Zad.2 
+//const multiplay = (a, b = 1) =>;
+
+//Zad.3
+//const average = (...args) =>
 
 
+//Zad.4
+//const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+//const result = average(...grades);
+
+//console.log(result);
+
+//Zad.5
+const array = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , ,firstname, ,lastname] = array;
+
+console.log(firstname,lastname);
