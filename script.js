10e383e50ad7002bb3ac4c90d44c89de799a10c3
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-05-14 12:01:13 +0200

    Add content to ex.2-4

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ console.log(all)
 
 
 //Zad.2 
-//const multiplay = (a, b = 1) =>;
+const multiplay = (a, b = 1) => a*b;
+console.log(multiplay(5));
+console.log(multiplay(5,5));
+
 
 //Zad.3
-//const average = (...args) =>
+const average = (...arg) => arg.reduce((a, b) => a + b)/arg.length;
+console.log(average(1, 3));
+console.log(average(1, 3, 6, 6));
+
 
 
 //Zad.4
-//const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-//const result = average(...grades);
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+const result = average(...grades);
+console.log(result);
 
-//console.log(result);
+
 
 //Zad.5
 const array = [1, 4, 'Iwona', false, 'Nowak'];
